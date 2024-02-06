f1da4d56489823331247a5699e04333567d59636
jmill215 <mil23019@byui.edu>
2024-02-05 20:22:03 -0700

    finished learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> ShapeList = new List<Shape>();
+
+        Square newSquare = new Square("red", 8);
+        ShapeList.Add(newSquare);
+        
+        // Old testing code from beginning of practice
+        //Console.WriteLine(newSquare.GetColor());
+        //Console.WriteLine(newSquare.GetArea());
+
+        Rectangle newRectangle = new Rectangle("blue", 10, 30);
+        ShapeList.Add(newRectangle);
+        
+        // Old testing code from beginning of practice
+        //Console.WriteLine(newRectangle.GetColor());
+        //Console.WriteLine(newRectangle.GetArea());
+
+        Circle newCircle = new Circle("blue", 10);
+        ShapeList.Add(newCircle);
+        
+        // Old testing code from beginning of practice
+        //Console.WriteLine(newCircle.GetColor());
+        //Console.WriteLine(newCircle.GetArea());
+
+        foreach(Shape s in ShapeList)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
